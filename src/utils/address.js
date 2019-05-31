6f464de1638045369e80c58495ddcc40c68fc17b
ducnd <duc.nguyen@midasprotocol.io>
2019-05-31 10:44:47 +0700

    change address prefix & package name

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,4 +1,4 @@
 export const ADDRESS_SIZE = 34;
-export const ADDRESS_PREFIX = "41";
-export const ADDRESS_PREFIX_BYTE = 0x41;
-export const ADDRESS_PREFIX_REGEX = /^(41)/;
+export const ADDRESS_PREFIX = "32";
+export const ADDRESS_PREFIX_BYTE = 0x32;
+export const ADDRESS_PREFIX_REGEX = /^(32)/;
